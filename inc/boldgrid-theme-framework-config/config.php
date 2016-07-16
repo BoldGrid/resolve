1475b2688ada7a55c8aeba814cf3e7efaf687a3c
Tim <dev@tim.ph>
2016-07-16 19:50:19 -0400

    updating container configs.

--- inc/boldgrid-theme-framework-config/config.php
+++ inc/boldgrid-theme-framework-config/config.php
@@ -25,11 +25,8 @@ function boldgrid_theme_framework_config( $boldgrid_framework_configs ) {
 	// Select the footer template to use.
 	$boldgrid_framework_configs['template']['footer'] = 'generic';
 
-	// Give page_home.php a fluid container.
-	$boldgrid_framework_configs['template']['pages']['page_home.php']['container_class'] = 'container-fluid';
-
-	// Give the default page template a container.
-	$boldgrid_framework_configs['template']['pages']['default']['container_class'] = 'container';
+	// Content sets containers in this theme.
+	$boldgrid_framework_configs['template']['pages']['page_home.php']['entry-content'] = '';
 
 	// Assign Locations for Generic Header.
 	$boldgrid_framework_configs['template']['locations']['header'] = array(
